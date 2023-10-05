068f72c8bf3955ab30fb0ded2a0fa62226ecfa26
REGSYBABA <sumimichael77@gmail.com>
2023-10-05 20:40:11 +0100

    Just modified the loading page function to suit the new functions from other tabs

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,10 +1,12 @@
 import createResturantHomePage from './restaurant'
 import createTabs from './tabs'
+// import createResturantHomePage2 from './resturant2'
 
 
 function initialLoad() {
     createTabs()
     createResturantHomePage()
+    // createResturantHomePage2()
 }
 
 export default initialLoad
