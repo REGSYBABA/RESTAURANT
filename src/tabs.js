21bee62317bcf774b61205fa1379cbd4e6335bd9
REGSYBABA <sumimichael77@gmail.com>
2023-10-05 20:42:43 +0100

    Little chnages on the buuton, making it look finer

--- src/tabs.js
+++ src/tabs.js
@@ -24,7 +24,7 @@ const createTabs = () => {
     div2.setAttribute('id', 'menu-btn')
     div3.setAttribute('id', 'conact-btn')
 
-    div1.classList.add('tab')
+    div1.classList.add('tab', 'active')
     div2.classList.add('tab')
     div3.classList.add('tab')
 
@@ -39,28 +39,45 @@ const createTabs = () => {
     pageContent.appendChild(div2)
     pageContent.appendChild(div3)
 
-    div1.addEventListener('click', () => {
+    div1.addEventListener('click', (e) => {
         clearContent()
+        if (e.target.classList.contains("active")) return;
+        activeButton(div1);
         createResturantHomePage()
     })
-    div2.addEventListener('click', () => {
+    div2.addEventListener('click', (e) => {
         clearContent()
+        if (e.target.classList.contains("active")) return;
+        activeButton(div2);
         createMenus()
     })
-    div3.addEventListener('click', () => {
+    div3.addEventListener('click', (e) => {
         clearContent()
+        if (e.target.classList.contains("active")) return;
+        activeButton(div3);
         createContactPage()
     })
 }
 
 function clearContent() {
     const content = document.querySelector("#content")
-    const header = document.querySelector(".header")
     const pageContent = document.querySelector('.page-content')
     if (pageContent) {
         content.removeChild(pageContent)
     }
 }
 
+
+function activeButton(button) {
+    const buttons = document.querySelectorAll(".tab");
+
+    buttons.forEach((button) => {
+        if (button !== this) {
+            button.classList.remove("active");
+        }
+    });
+
+    button.classList.add("active");
+}
 
 export default createTabs
